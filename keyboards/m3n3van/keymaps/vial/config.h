de5f92447bbe4156bb2f1d68663532b3920e5f65
Dan Hoizner <dan.hoizner@gmail.com>
2021-10-18 11:57:06 +0300

    add m3n3van vial

---
+++ keyboards/m3n3van/keymaps/vial/config.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xF0, 0xD2, 0x9C, 0x5E, 0x75, 0xF4, 0x3D, 0x32}
+
+#define VIAL_UNLOCK_COMBO_COLS { 0, 1 }
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 12 }
