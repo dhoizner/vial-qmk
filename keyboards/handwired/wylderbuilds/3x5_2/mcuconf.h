0da0815b6d9d649a2df5a0eaf8f7b3ff1c5bbc9b
Dan Hoizner <dan.hoizner@gmail.com>
2023-11-24 18:05:03 -0500

    resolve qmk lint warnings on 3x5_2

--- keyboards/handwired/wylderbuilds/3x5_2/mcuconf.h
+++ keyboards/handwired/wylderbuilds/3x5_2/mcuconf.h
@@ -4,7 +4,7 @@
 
 #pragma once
 
-#include_next "mcuconf.h"
+#include_next <mcuconf.h>
 
 #undef RP_I2C_USE_I2C0
 #define RP_I2C_USE_I2C0 TRUE
@@ -13,6 +13,6 @@
 #define RP_I2C_USE_I2C1 FALSE
 
 #ifndef WYLD_QMK_FIRMWARE_MCUCONF_H
-#define WYLD_QMK_FIRMWARE_MCUCONF_H
+#    define WYLD_QMK_FIRMWARE_MCUCONF_H
 
 #endif // WYLD_QMK_FIRMWARE_MCUCONF_H
